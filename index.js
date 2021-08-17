b81e2aa24352c6ff1591ad8eabf287c3cc38ce83
Nissrine Canina <niss.canina@gmail.com>
2021-08-17 11:22:23 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+//document.body.append(newHeader);
+
+//const newHeader = document.querySelector("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "<h1>Nissrine is the champion</h1>";
+
+
+/*const victory = document.querySelector("#victory");
+victory.innerHTML = "<h1>Nissrine is the champion</h1>";*/
+/*newHeader.append(h1);
+
+newHeader.id = "victory";
+const victory = document.getElementById("victory");
+
+document.getElementById("victory").innerHTML = "Nissrine is the champion";*/
